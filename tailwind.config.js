3d6038d5d2817e88682da3635dc43b3614147316
swanagan <swanaganray@gmail.com>
2024-10-23 15:43:27 -0400

    trying to fix prod styling problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}', // Include the app directory
-    './Components/**/*.{js,ts,jsx,tsx}', // Include Components folder
+    './components/**/*.{js,ts,jsx,tsx}', // Include Components folder
     './pages/**/*.{js,ts,jsx,tsx}', // Include any other pages
     './public/**/*.html',
   ],
